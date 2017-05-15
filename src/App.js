8302a53a9b61a15f42f181de78c2b3e60e97f1f6
Felix Su <felixs8696@berkeley.edu>
2017-05-15 15:56:50 -0400

    updated profile picture

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// var topics = {
+//   'coder': 
+// }
+
+function TitlePanel(props) {
+  return(
+    <div className="title-container">
+      <h1 className="title">Hi there! I'm Felix.</h1>
+      <p className="intro">What would you like to learn about me?</p>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="background">
-          <div className="panel left profile-photo">
+          <div className="panel right interest-photo">
+            <TitlePanel></TitlePanel>
           </div>
-          <div className="panel right">
-            <div className="title-container">
-              <h1 className="title">Hi there! I'm Felix.</h1>
-              <p className="intro">What would you like to learn about me?</p>
-            </div>
+          <div className="panel left profile-photo">
           </div>
         </div>
       </div>
