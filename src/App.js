ecc043bc092cfcb41429e528665fa4fce64a8c3d
Felix Su <felixs8696@berkeley.edu>
2017-05-15 05:24:09 -0400

    basic minimalist cover page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="background">
+          <div className="panel left profile-photo">
+          </div>
+          <div className="panel right">
+            <div className="title-container">
+              <h1 className="title">Hi there! I'm Felix.</h1>
+              <p className="intro">What would you like to learn about me?</p>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
