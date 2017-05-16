dd8d30fdb8e83f097dbb51570c473718beab45a6
Felix Su <felixs8696@berkeley.edu>
2017-05-16 05:46:36 -0400

    decided on navbar hover style

--- src/App.js
+++ src/App.js
@@ -44,32 +44,40 @@ function PortraitPanel(props) { return(<div className="panel left profile-photo"
 
 // Right Panels
 var TitlePanel = Radium(TitlePanelContent)
-var OptionsPanel = Radium(OptionsPanelContent)
+var OptionsPanel = Radium(AboutPanelContent)
 
 function TitlePanelContent(props) {
   return(
     <div className="panel right">
       <div className="title-container">
           <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
-          <div style={[styles.fadeInDownDelay]} onClick={() => { props.changePanel((<PortraitPanel></PortraitPanel>), (<OptionsPanel></OptionsPanel>)) }}>
-            <Button waves='light' className='blue'>Click to learn more about me</Button>
-          </div>
+          <p style={[styles.fadeInDownDelay]}>Hover over or click an icon to learn more about me</p>
       </div>
     </div>
   )
 }
 
-function OptionsPanelContent(props) {
+function AboutPanelContent(props) {
   return(
     <div className="panel right interest-photo" style={[styles.fadeInDownSlow]}>
-      
+      <div className="title-container">
+        <p>Hey I'm Felix! I am a 3rd year pursuing my <strong>Computer Science</strong> degree at <a href="http://www.berkeley.edu" target="_blank">UC Berkeley</a>. I am a dreamer at heart, builder by nature, and coder by trade. My favorite part of being an engineer is having the ability to react to and solve problems with minimal resources.</p>
+        <p>I love to code, but if I don't have my laptop on hand, I am probably shooting hoops, jamming with my friends (sorry neighbors), or going out to meet new people. Ever since we lost fellow student and friend Nick Leslie in the Nice attacks in the summer of 2016, I have been inspired by him to live spontaneously rather than sit still. So, you can expect to find me out and about finding new things to do and people to be around.</p>
+        <p>Software development and product management are my main competencies. I have 3 years of software engineering experience in both frontend and backend using web frameworks and databases. After getting a feel for various engineering fields, I have taken up <strong>machine learning</strong>, <strong>artificial intelligence</strong>, and <strong>computer vision</strong> as my main interests. If you want to check out what I have done so far just click the next button below to scroll through my experiences or click for <a href="/resumes/felixsu_resume_sp17.pdf" target="_blank">my resume</a>.</p>
+        <p>As I continue my journey, I love talking with and helping out fellow engineers, so please don't hesitate to reach out if you want to chat about ways to move the industry forward and about creating things that will help improve lives.</p>
+      </div>
+    </div>
+  )
+}
+
+function NavBar(props) {
+  return(
+    <div className="navbar">
     </div>
   )
 }
 
 class App extends Component {
-
-
   constructor(props) {
     super(props);
     this.changePanel = this.changePanel.bind(this);
