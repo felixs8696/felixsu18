39332a37c1715644d23fc7c6df0bc82a88819d02
Felix Su <felixs8696@berkeley.edu>
2017-05-15 21:08:25 -0400

    added panel state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { fadeInDown } from 'react-animations';
+import { fadeInDown, slideUp } from 'react-animations';
 import Radium from 'radium';
 import {StyleRoot} from 'radium';
 import { Button } from 'react-materialize';
@@ -16,43 +16,60 @@ const styles = {
     animationDelay: '.65s',
     animationFillMode: 'forwards',
     animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+  },
+  slideUp: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideUp, 'slideUp')
   }
 }
 
-function TitlePanel(props) {
+// Left Panels
+function PortraitPanel(props) { return(<div className="panel left profile-photo"></div>) }
+
+// Right Panels
+var TitlePanel = Radium(TitlePanelContent)
+var OptionsPanel = Radium(OptionsPanelContent)
+
+function TitlePanelContent(props) {
   return(
-    <div className="title-container">
-        <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
-        {/* <p className="intro" style={[styles.fadeInDownDelay]}>What would you like to learn about me?</p> */}
-        <div style={[styles.fadeInDownDelay]}><Button waves='light' className='blue'>Click to learn more about me</Button></div>
+    <div className="panel right interest-photo">
+      <div className="title-container">
+          <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
+          <div style={[styles.fadeInDownDelay]}><Button waves='light' className='blue'>Click to learn more about me</Button></div>
+      </div>
     </div>
   )
 }
 
-function OptionsPanel(props) {
+function OptionsPanelContent(props) {
   return(
-    <div className="title-container">
-        <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
-        <p className="intro" style={[styles.fadeInDownDelay]}>What would you like to learn about me?</p>
-        <Button waves='light' className='blue'>Click to learn more about me.</Button>
+    <div className="panel right">
+      <div className="title-container">
+          <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
+          <p className="intro" style={[styles.fadeInDownDelay]}>What would you like to learn about me?</p>
+          <Button waves='light' className='blue'>Click to learn more about me.</Button>
+      </div>
     </div>
   )
 }
-
-var Intro = Radium(TitlePanel)
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftPanel: (<PortraitPanel></PortraitPanel>),
+      rightPanel: (<TitlePanel></TitlePanel>)
+    };
+  }
+
   render() {
     return (
       <StyleRoot>
         <div className="App">
           <img src="/img/logo.png" className="App-logo" alt="logo" />
           <div className="background">
-            <div className="panel right interest-photo">
-              <Intro></Intro>
-            </div>
-            <div className="panel left profile-photo">
-            </div>
+            {this.leftPanel}
+            {this.rightPanel}
           </div>
         </div>
       </StyleRoot>
