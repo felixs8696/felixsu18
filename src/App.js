aca83346ea76e9a41f26d30c5c971641e7b40bb7
Felix Su <felixs8696@berkeley.edu>
2017-05-16 05:10:11 -0400

    replaceable panels

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import { fadeInDown, slideUp } from 'react-animations';
+import { fadeInUp, fadeOutUp, fadeInDown, slideInDown } from 'react-animations';
 import Radium from 'radium';
 import {StyleRoot} from 'radium';
 import { Button } from 'react-materialize';
 
 const styles = {
-  fadeInDown: {
+  fadeInUp: {
     animation: 'x 1s',
+    animationName: Radium.keyframes(fadeInUp, 'fadeInUp')
+  },
+  fadeOutUp: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeOutUp, 'fadeOutUp')
+  },
+  fadeInDown: {
+    opacity: '0',
+    animation: 'x 1s',
+    animationFillMode: 'forwards',
+    animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+  },
+  fadeInDownSlow: {
+    opacity: '0',
+    animation: 'x 1.6s',
+    animationFillMode: 'forwards',
     animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
   },
   fadeInDownDelay: {
@@ -17,9 +33,9 @@ const styles = {
     animationFillMode: 'forwards',
     animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
   },
-  slideUp: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(slideUp, 'slideUp')
+  slideInDown: {
+    animation: 'x 2s',
+    animationName: Radium.keyframes(slideInDown, 'slideInDown')
   }
 }
 
@@ -32,10 +48,12 @@ var OptionsPanel = Radium(OptionsPanelContent)
 
 function TitlePanelContent(props) {
   return(
-    <div className="panel right interest-photo">
+    <div className="panel right">
       <div className="title-container">
           <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
-          <div style={[styles.fadeInDownDelay]}><Button waves='light' className='blue'>Click to learn more about me</Button></div>
+          <div style={[styles.fadeInDownDelay]} onClick={() => { props.changePanel((<PortraitPanel></PortraitPanel>), (<OptionsPanel></OptionsPanel>)) }}>
+            <Button waves='light' className='blue'>Click to learn more about me</Button>
+          </div>
       </div>
     </div>
   )
@@ -43,23 +61,30 @@ function TitlePanelContent(props) {
 
 function OptionsPanelContent(props) {
   return(
-    <div className="panel right">
-      <div className="title-container">
-          <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
-          <p className="intro" style={[styles.fadeInDownDelay]}>What would you like to learn about me?</p>
-          <Button waves='light' className='blue'>Click to learn more about me.</Button>
-      </div>
+    <div className="panel right interest-photo" style={[styles.fadeInDownSlow]}>
+      
     </div>
   )
 }
 
 class App extends Component {
+
+
   constructor(props) {
     super(props);
+    this.changePanel = this.changePanel.bind(this);
+
     this.state = {
       leftPanel: (<PortraitPanel></PortraitPanel>),
-      rightPanel: (<TitlePanel></TitlePanel>)
+      rightPanel: (<TitlePanel changePanel={this.changePanel}></TitlePanel>)
     };
+  }
+
+  changePanel(newLeftPanel, newRightPanel) {
+    this.setState(prevState => ({
+      leftPanel: newLeftPanel,
+      rightPanel: newRightPanel
+    }));
   }
 
   render() {
@@ -68,8 +93,8 @@ class App extends Component {
         <div className="App">
           <img src="/img/logo.png" className="App-logo" alt="logo" />
           <div className="background">
-            {this.leftPanel}
-            {this.rightPanel}
+            {this.state.leftPanel}
+            {this.state.rightPanel}
           </div>
         </div>
       </StyleRoot>
