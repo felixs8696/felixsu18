fd110d4627366dfe9eff85a34db2ab5a8ec27fd5
Felix Su <felixs8696@berkeley.edu>
2017-05-15 20:37:38 -0400

    finished logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { fadeInDown } from 'react-animations';
+import Radium from 'radium';
+import {StyleRoot} from 'radium';
+import { Button } from 'react-materialize';
 
-// var topics = {
-//   'coder': 
-// }
+const styles = {
+  fadeInDown: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+  },
+  fadeInDownDelay: {
+    opacity: '0',
+    animation: 'x 1s',
+    animationDelay: '.65s',
+    animationFillMode: 'forwards',
+    animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+  }
+}
 
 function TitlePanel(props) {
   return(
     <div className="title-container">
-      <h1 className="title">Hi there! I'm Felix.</h1>
-      <p className="intro">What would you like to learn about me?</p>
+        <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
+        {/* <p className="intro" style={[styles.fadeInDownDelay]}>What would you like to learn about me?</p> */}
+        <div style={[styles.fadeInDownDelay]}><Button waves='light' className='blue'>Click to learn more about me</Button></div>
     </div>
   )
 }
 
+function OptionsPanel(props) {
+  return(
+    <div className="title-container">
+        <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
+        <p className="intro" style={[styles.fadeInDownDelay]}>What would you like to learn about me?</p>
+        <Button waves='light' className='blue'>Click to learn more about me.</Button>
+    </div>
+  )
+}
+
+var Intro = Radium(TitlePanel)
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="background">
-          <div className="panel right interest-photo">
-            <TitlePanel></TitlePanel>
-          </div>
-          <div className="panel left profile-photo">
+      <StyleRoot>
+        <div className="App">
+          <img src="/img/logo.png" className="App-logo" alt="logo" />
+          <div className="background">
+            <div className="panel right interest-photo">
+              <Intro></Intro>
+            </div>
+            <div className="panel left profile-photo">
+            </div>
           </div>
         </div>
-      </div>
+      </StyleRoot>
     );
   }
 }
