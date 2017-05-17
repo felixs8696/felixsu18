93898237c14861e5c20c0127ad6f103d2bbf8a05
Felix Su <felixs8696@berkeley.edu>
2017-05-16 21:06:25 -0400

    switch out about panel

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import FaTerminal from 'react-icons/lib/fa/terminal';
 import FaGraduationCap from 'react-icons/lib/fa/graduation-cap';
 import FaCameraRetro from 'react-icons/lib/fa/camera-retro';
 import FaMusic from 'react-icons/lib/fa/music';
-import { fadeInUp, fadeOutUp, fadeInDown, slideInDown } from 'react-animations';
+import { fadeIn, fadeInUp, fadeOutUp, fadeInDown, slideInDown } from 'react-animations';
 import Radium from 'radium';
 import {StyleRoot} from 'radium';
 import { Button } from 'react-materialize';
 
 const styles = {
+  fadeIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  },
   fadeInUp: {
     animation: 'x 1s',
     animationName: Radium.keyframes(fadeInUp, 'fadeInUp')
@@ -49,12 +53,12 @@ function PortraitPanel(props) { return(<div className="panel left profile-photo"
 
 // Right Panels
 var TitlePanel = Radium(TitlePanelContent)
-var OptionsPanel = Radium(AboutPanelContent)
+var AboutPanel = Radium(AboutPanelContent)
 
 function TitlePanelContent(props) {
   return(
     <div className="panel right">
-      <div className="title-container">
+      <div className="center-container center">
           <h1 className="title" style={[styles.fadeInDown]}>Hi there! I'm Felix.</h1>
           <p style={[styles.fadeInDownDelay]}>Hover over or click an icon to learn more about me</p>
       </div>
@@ -64,12 +68,14 @@ function TitlePanelContent(props) {
 
 function AboutPanelContent(props) {
   return(
-    <div className="panel right interest-photo" style={[styles.fadeInDownSlow]}>
-      <div className="title-container">
-        <p>Hey I'm Felix! I am a 3rd year pursuing my <strong>Computer Science</strong> degree at <a href="http://www.berkeley.edu" target="_blank">UC Berkeley</a>. I am a dreamer at heart, builder by nature, and coder by trade. My favorite part of being an engineer is having the ability to react to and solve problems with minimal resources.</p>
-        <p>I love to code, but if I don't have my laptop on hand, I am probably shooting hoops, jamming with my friends (sorry neighbors), or going out to meet new people. Ever since we lost fellow student and friend Nick Leslie in the Nice attacks in the summer of 2016, I have been inspired by him to live spontaneously rather than sit still. So, you can expect to find me out and about finding new things to do and people to be around.</p>
-        <p>Software development and product management are my main competencies. I have 3 years of software engineering experience in both frontend and backend using web frameworks and databases. After getting a feel for various engineering fields, I have taken up <strong>machine learning</strong>, <strong>artificial intelligence</strong>, and <strong>computer vision</strong> as my main interests. If you want to check out what I have done so far just click the next button below to scroll through my experiences or click for <a href="/resumes/felixsu_resume_sp17.pdf" target="_blank">my resume</a>.</p>
-        <p>As I continue my journey, I love talking with and helping out fellow engineers, so please don't hesitate to reach out if you want to chat about ways to move the industry forward and about creating things that will help improve lives.</p>
+    <div className="panel right" style={[styles.fadeIn]}>
+      <div className="center-container">
+        <div className="about">
+          <p>Hey I'm Felix! I am a 3rd year pursuing my <strong>Computer Science</strong> degree at <a href="http://www.berkeley.edu" target="_blank">UC Berkeley</a>. I am a dreamer at heart, builder by nature, and coder by trade. My favorite part of being an engineer is having the ability to react to and solve problems with minimal resources.</p>
+          <p>I love to code, but if I don't have my laptop on hand, I am probably shooting hoops, jamming with my friends (sorry neighbors), or going out to meet new people. Ever since we lost fellow student and friend Nick Leslie in the Nice attacks in the summer of 2016, I have been inspired by him to live spontaneously rather than sit still. So, you can expect to find me out and about finding new things to do and people to be around.</p>
+          <p>Software development and product management are my main competencies. I have 3 years of software engineering experience in both frontend and backend using web frameworks and databases. After getting a feel for various engineering fields, I have taken up <strong>machine learning</strong>, <strong>artificial intelligence</strong>, and <strong>computer vision</strong> as my main interests. If you want to check out what I have done so far just click the next button below to scroll through my experiences or click for <a href="/resumes/felixsu_resume_sp17.pdf" target="_blank">my resume</a>.</p>
+          <p>As I continue my journey, I love talking with and helping out fellow engineers, so please don't hesitate to reach out if you want to chat about ways to move the industry forward and about creating things that will help improve lives.</p>
+        </div>
       </div>
     </div>
   )
@@ -78,7 +84,11 @@ function AboutPanelContent(props) {
 function NavBar(props) {
   return(
     <div className="navbar">
-      <Button className="circle white"><FaHeartO/></Button>
+      <Button className="circle white"
+              onMouseOver={ () => { props.changeRightPanel((<AboutPanel/>)) } }
+              onMouseLeave= { () => { props.changeRightPanel((<TitlePanel/>)) } }>
+        <FaHeartO/>
+      </Button>
       <Button className="circle white"><FaTerminal/></Button>
       <Button className="circle white"><FaGraduationCap/></Button>
       <Button className="circle white"><FaMusic/></Button>
@@ -92,16 +102,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.changePanel = this.changePanel.bind(this);
+    this.changeRightPanel = this.changeRightPanel.bind(this);
 
     this.state = {
-      leftPanel: (<PortraitPanel></PortraitPanel>),
-      rightPanel: (<TitlePanel changePanel={this.changePanel}></TitlePanel>)
+      leftPanel: (<PortraitPanel/>),
+      rightPanel: (<TitlePanel/>)
     };
   }
 
   changePanel(newLeftPanel, newRightPanel) {
     this.setState(prevState => ({
       leftPanel: newLeftPanel,
+      rightPanel: newRightPanel
+    }));
+  }
+
+  changeRightPanel(newRightPanel) {
+    this.setState(prevState => ({
       rightPanel: newRightPanel
     }));
   }
@@ -113,7 +130,7 @@ class App extends Component {
           <img src="/img/logo.png" className="App-logo" alt="logo" />
           <div className="background">
             {this.state.leftPanel}
-            <NavBar />
+            <NavBar changeRightPanel={this.changeRightPanel}/>
             {this.state.rightPanel}
           </div>
         </div>
